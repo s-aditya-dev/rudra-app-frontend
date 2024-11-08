14940a6af75da332bc7485b6eef8ef1f5619508b
s-aditya-dev <sraditya1203@gmail.com>
2024-11-09 00:30:37 +0530

    changes done in setting.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
-// export const frontEndPort = 'https://rudra-app-frontend-h0cv.onrender.com';
-// export const backEndPort = 'https://rudra-app-backend-7geo.onrender.com';
+export const frontEndPort = "https://rudra-app-frontend.vercel.app";
+export const backEndPort = "https://rudra-app-backend.vercel.app";
 
 // local config.js
-export const frontEndPort = "http://localhost:5173";
-export const backEndPort = "http://localhost:8000";
+// export const frontEndPort = "http://localhost:5173";
+// export const backEndPort = "http://localhost:8000";
