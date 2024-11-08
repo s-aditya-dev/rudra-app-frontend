01c5d2f4054153433a29dc787f9783983fdabb10
s-aditya-dev <sraditya1203@gmail.com>
2024-11-08 22:16:19 +0530

    added settings.js correct path

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { backEndPort } from "@/settings.js";
+import { backEndPort } from "../../settings.js";
 
 const newRequest = axios.create({
   baseURL: `${backEndPort}/api/`,
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { backEndPort } from "@/settings.js";
+import { backEndPort } from "./settings.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
